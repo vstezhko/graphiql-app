b1d1edd7f17fb3c2f8967b1e7dcd115be03bb00d
Kseniya Biarezina <ksenia-2010@tut.by>
2023-12-12 11:20:39 +0100

    GE-28 refactor: remove redundant code

--- src/components/localizationComponent/Localization.tsx
+++ src/components/localizationComponent/Localization.tsx
@@ -1,7 +1,86 @@
-import Popover from './Popover.tsx';
+import {
+  ListItemText,
+  MenuList,
+  MenuItem,
+  ListItemButton,
+  Button,
+  Popover,
+} from '@mui/material';
+import { useContext, useState } from 'react';
+import { LanguageContext, LanguageKeys } from '../../context/LanguageContext';
+
+type LangArrayObject = {
+  id: string;
+  name: string;
+  abbr: string;
+};
 
 const Localization = ({ scroll }: { scroll: boolean }) => {
-  return <Popover scroll={scroll} />;
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const { dictionary, language, setLanguage } = useContext(LanguageContext);
+
+  const langArray: LangArrayObject[] = [
+    { id: 'en', name: dictionary.english, abbr: dictionary.en },
+    { id: 'ru', name: dictionary.russian, abbr: dictionary.ru },
+  ];
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleListItemClick = (lang: LanguageKeys) => {
+    setLanguage(lang);
+    handleClose();
+  };
+
+  const id = Boolean(anchorEl) ? 'simple-popover' : undefined;
+
+  return (
+    <div>
+      <Button
+        aria-describedby={id}
+        variant="contained"
+        onClick={handleClick}
+        className="language__button"
+      >
+        {langArray.find(({ id }) => id === language)?.abbr}
+      </Button>
+      <Popover
+        disableScrollLock
+        id={id}
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorReference={'anchorPosition'}
+        anchorPosition={{
+          left: window.innerWidth - 200,
+          top: scroll ? 70 : 110,
+        }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+      >
+        <MenuList>
+          {langArray.map(({ id, name }) => (
+            <MenuItem key={id}>
+              <ListItemButton
+                disableRipple
+                selected={language === id}
+                onClick={() => handleListItemClick(id as LanguageKeys)}
+              >
+                <ListItemText primary={name} />
+              </ListItemButton>
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Popover>
+    </div>
+  );
 };
 
 export default Localization;
