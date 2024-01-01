0b03a2d2f7221d6a43a45f506de314f3edc675db
viktoriastezko <viktoriastezhko@gmail.com>
2024-01-01 19:35:01 +0300

    GE-26 feat: add main info abour developers

---
+++ src/components/ThankSection.tsx
@@ -0,0 +1,35 @@
+export const ThanksSection = () => {
+  return (
+    <div className="thanks">
+      <div className="thanks__image-block">
+        {/*<Link className='thanks__link' href='https://rs.school/'>*/}
+        {/*  <SchoolLogo />*/}
+        {/*</Link>*/}
+      </div>
+      <div className="thanks__text-block">
+        <p>
+          Our project &apos;s journey began within the nurturing confines of RS
+          School, a program founded by
+          <span className="highlight">
+            {' '}
+            The Rolling Scopes developer community
+          </span>{' '}
+          in 2013.
+        </p>
+        <p>
+          It&apos;s at RS School that we found{' '}
+          <span className="highlight">the inspiration and skills</span> needed
+          to bring this project to life. This school not only equipped us with{' '}
+          <span className="highlight">the technical know-how</span> but also
+          served as{' '}
+          <span className="highlight">the meeting point for our team</span>.
+        </p>
+        <p>
+          We are immensely grateful to the mentors who{' '}
+          <span className="highlight">generously shared their expertise</span>,
+          making this project possible.
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/pages/welcomPage/WelcomePage.tsx
+++ src/pages/welcomPage/WelcomePage.tsx
@@ -1,5 +1,116 @@
+import { TeamMemberCard } from '../../components/TeamMemberCard.tsx';
+import { ThanksSection } from '../../components/ThankSection.tsx';
+
+type SkillItem = [string, number];
+
+export interface TeamMemberParams {
+  name: string;
+  role: string;
+  text: string;
+  highlights: string[];
+  photo: string;
+  githubLink: string;
+  linkedinLink: string;
+  skills: SkillItem[];
+  contribution: string;
+}
+
+const TeamMembersData: TeamMemberParams[] = [
+  {
+    name: 'Kseniya Biarezina',
+    role: 'Front-end Dev',
+    text: `While working as a financial analyst in the banking sector, I made extensive use of Excel, SQL, and VBA. I transitioned then to web development through courses on LinkedIn Learning and Netology, and RS School's Stage 0. Despite being new to commercial web development, I've been involved in numerous projects during my training. I find web development exciting for its tangible results and constant learning opportunities.`,
+    highlights: [
+      'Excel, SQL, and VBA',
+      'LinkedIn Learning',
+      'Netology',
+      `RS School's Stage 0`,
+      'tangible results',
+      'constant learning opportunities',
+    ],
+    photo: '../../../src/assets/team-members/ksusha.jpg',
+    githubLink: 'https://github.com/BiarezKseniya',
+    linkedinLink: 'https://www.linkedin.com/in/kseniya-biarezina/',
+    skills: [
+      ['requeriment analysis', 95],
+      ['learning', 100],
+      ['outcomes delivery', 90],
+    ],
+    contribution: 'taking part in coding, realize task requirements, testing',
+  },
+  {
+    name: 'Eugenia Khaleeva',
+    role: 'Front-end Dev',
+    text: `I'm a React Front-end Developer with over 1 year of commercial development experience. My goal is to become a sought-after IT specialist by continually improving my skills. I'm known for my teamwork and problem-solving abilities. I have a background in account management and certifications in Front-end development, including completion of the JS/FE PRE-SCHOOL program.`,
+    highlights: [
+      'over 1 year of commercial development experience',
+      'become a sought-after IT specialist',
+      'my teamwork and problem-solving abilities',
+      'certifications in Front-end development',
+      'JS/FE PRE-SCHOOL program',
+    ],
+    photo: '../../../src/assets/team-members/zhenya.jpg',
+    githubLink: 'https://github.com/khaleeva',
+    linkedinLink: 'https://www.linkedin.com/in/eugenia-khaleeva-b66035157',
+    skills: [
+      ['creativity', 100],
+      ['teamwork', 90],
+      ['problem-solving', 95],
+    ],
+    contribution: 'taking part in coding, realize task requirements, design',
+  },
+  {
+    name: 'Viktoria Stezhko',
+    role: 'Team-Lead | Front-end Dev',
+    text: `I'm a creative and adaptable professional with excellent communication skills. After gaining valuable experience in management and receiving positive reviews for streamlining business processes, I moved into the IT industry, where I've been working for 1.5 years. My experience includes notable projects such as a website for an online film festival and a web app for B2B sales platform.`,
+    highlights: [
+      'excellent communication skills',
+      'experience in management',
+      'streamlining business processes',
+      'a website for an online film festival',
+      'a web app for B2B sales platform',
+    ],
+    photo: '../../../src/assets/team-members/vika.jpg',
+    githubLink: 'https://github.com/vstezhko',
+    linkedinLink: 'https://www.linkedin.com/in/viktoria-stezhko',
+    skills: [
+      ['communication', 95],
+      ['process improvement', 90],
+      ['project management', 100],
+    ],
+    contribution:
+      'taking part in coding, realize task requirements, optimization',
+  },
+];
+
 const WelcomePage = () => {
-  return <div>welcome Page</div>;
+  return (
+    <>
+      <div className="about__item">
+        <h3>Meet our team</h3>
+        <p>
+          Our team of developers excelled through active and effective
+          communication. We held regular meetings, discussed our ideas, and
+          shared best practices, allowing us to reach a consensus and create a
+          high-quality product. Our openness to feedback and ability to work as
+          a team made our project successful and satisfied our clients needs.
+        </p>
+        <div className="about__team-members">
+          {TeamMembersData.map((member, index) => (
+            <TeamMemberCard
+              key={index}
+              teamMemberData={member}
+              isEven={index % 2 === 0}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="about__item">
+        <h3>Roots in RS School</h3>
+        <ThanksSection />
+      </div>
+    </>
+  );
 };
 
 export default WelcomePage;
