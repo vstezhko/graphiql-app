e8cf92eb3da28d2f4dfa487bd56f61071f6a394d
viktoriastezko <viktoriastezhko@gmail.com>
2024-01-07 13:38:52 +0300

    feat GE-42: add test error boundary

--- src/__tests__/errorBoundary.test.tsx
+++ src/__tests__/errorBoundary.test.tsx
@@ -4,6 +4,9 @@ import ErrorBoundary from '../components/ErrorBoundary.tsx';
 const FallbackComponent = () => <div>Fallback Component</div>;
 
 const ChildComponent = () => <div>Child Component</div>;
+const ChildComponentWithError = () => {
+  throw new Error('Custom error');
+};
 
 describe('ErrorBoundary', () => {
   it('renders children when there is no error', () => {
@@ -15,5 +18,14 @@ describe('ErrorBoundary', () => {
 
     expect(getByText('Child Component')).toBeInTheDocument();
     expect(queryByText('Fallback Component')).not.toBeInTheDocument();
+  });
+  it('renders children when there is no error', () => {
+    const { queryByText } = render(
+      <ErrorBoundary fallback={<FallbackComponent />}>
+        <ChildComponentWithError />
+      </ErrorBoundary>
+    );
+
+    expect(queryByText('Fallback Component')).toBeInTheDocument();
   });
 });
