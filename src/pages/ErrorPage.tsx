160d6552aed991215f73b162e94a83e4265c18cb
viktoriastezko <viktoriastezhko@gmail.com>
2024-01-07 15:52:28 +0300

    fix: fix error page route

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,17 +1,14 @@
 import { useContext } from 'react';
 import { DictionaryKey, LanguageContext } from '../context/LanguageContext';
-import Layout from './../components/layout/Layout';
 
 const ErrorPage = ({ title, text }: { title: string; text: string }) => {
   const { dictionary } = useContext(LanguageContext);
   return (
-    <Layout>
-      <div className="container__img">
-        <img src="/img/error.png" alt="404" />
-        <h1>{dictionary[title as DictionaryKey]}</h1>
-        <p>{dictionary[text as DictionaryKey]}</p>
-      </div>
-    </Layout>
+    <div className="container__img">
+      <img src="/img/error.png" alt="404" />
+      <h1>{dictionary[title as DictionaryKey]}</h1>
+      <p>{dictionary[text as DictionaryKey]}</p>
+    </div>
   );
 };
 
